7f7facd15c0b6545d6478394e42cbfb286de2035
Andres Ramos <andres.f.salazar@correounivalle.edu.co>
2022-02-18 18:32:09 +0000

    Finish Introduction and Django Admin

--- blango/settings.py
+++ blango/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blango',
+    'blog',
 ]
 
 MIDDLEWARE = [
--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from blog.modells import Tag, Post
+from .models import Tag, Post
+
 
 admin.site.register(Tag)
-
-
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
-admin.site.register(Post, PostAdmin)
+admin.site.register(Post)
+
